3433e53b8d7c16386dcce9fd450c7ea1d9f84edd
Jordi Navarrete <jnavarrete@inboundemotion.com>
2026-01-12 03:22:44 -0500

    fix: Comment out organizationId field in clients schema - column doesn't exist in database
    Error: Unknown column 'organization_id' in 'field list'
    This field was defined in Drizzle schema but doesn't exist in the actual database table.

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -281,7 +281,7 @@ export const clients = mysqlTable("clients", {
 	longitude: decimal({ precision: 10, scale: 7 }),
 	routeGroup: varchar({ length: 50 }),
 	partnerId: int().default(1).notNull(),
-	organizationId: int("organization_id"),
+	// organizationId: int("organization_id"), // COMENTADO: Esta columna no existe en la BD
 });
 
 export const distributorModuleConfig = mysqlTable("distributor_module_config", {
