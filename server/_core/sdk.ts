70ae699290a7e09b801e9678d5fb73964c26bc9c
Jordi Navarrete <jnavarrete@inboundemotion.com>
2026-01-21 01:59:08 -0500

    fix: convertir Date a string en lastSignedIn para cumplir con el tipo esperado

--- server/_core/sdk.ts
+++ server/_core/sdk.ts
@@ -259,7 +259,7 @@ class SDKServer {
           name: userInfo.name || null,
           email: userInfo.email ?? null,
           loginMethod: userInfo.loginMethod ?? userInfo.platform ?? null,
-          lastSignedIn: signedInAt,
+          lastSignedIn: signedInAt.toISOString(),
         });
         user = await db.getUserByOpenId(userInfo.openId);
       } catch (error) {
@@ -274,7 +274,7 @@ class SDKServer {
 
     await db.upsertUser({
       openId: user.openId,
-      lastSignedIn: signedInAt,
+      lastSignedIn: signedInAt.toISOString(),
     });
 
     return user;
