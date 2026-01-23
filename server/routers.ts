81f8c84b1ddcb8ed1baa626898d076aa46202391
Jordi Navarrete <jnavarrete@inboundemotion.com>
2026-01-23 16:40:44 -0500

    fix: Eliminar código de predictions router que causaba errores

--- server/routers.ts
+++ server/routers.ts
@@ -49,7 +49,7 @@ import { shopRouter } from "./routers/shop/shop.router.js";
 import { teamRouter } from "./routers/team.router.js";
 import { clientRouter } from "./routers/crm/index.js";
 import { analyticsRouter } from "./routers/reports/index.js";
-import { predictionsRouter } from "./routers/predictions/predictions.router.js";
+
 
 export const appRouter = router({
   // Sistema
@@ -135,8 +135,6 @@ export const appRouter = router({
   // Reportes y Analytics
   analytics: analyticsRouter,
 
-  // Predicciones avanzadas
-  predictions: predictionsRouter,
 
 });
 
--- server/routers/predictions/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Módulo de Predicciones
- * Piano Emotion Manager
- */
-
-export { predictionsRouter } from './predictions.router.js';
-export type { PredictionsRouter } from './predictions.router.js';
